d0defec27f5f78a68e8bcf1439ddb32a4fb9bca3
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-11 12:40:17 +0100

    xcvxc
    sdfdasf

---
+++ application/models/userModel.php
@@ -0,0 +1,27 @@
+<?php
+	class userModel extends CI_Model {
+		// CONSTRUCTOR
+        public function __construct()
+        {
+                parent::__construct();
+        }
+
+		// BESTAAT GEBRUIKER?
+        public function exists($naam)
+        {
+            $query = $this->db->get_where('gebruiker', array('gebruikersnaam' => $naam));           
+        }
+
+		// LEERLING AANMAKEN
+        public function createLeerling($gebruikersnaam, $wachtwoord, $email)
+        {
+			$data = array(
+				"gebruikersnaam" => $gebruikersnaam,
+				"wachtwoord" => hash("sha256", $wachtwoord),
+				"email" => $email
+			);
+			
+            $this->db->insert('gebruiker', $data);
+        }
+
+	}

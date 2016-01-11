717990f2225501c4145da716195b753a0fa5a807
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-11 12:52:50 +0100

    asa

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'form', 'session', 'email', 'url');
+$autoload['libraries'] = array('database', 'session', 'email');
 
 /*
 | -------------------------------------------------------------------

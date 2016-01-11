40acd2540b2e4d38275083376d114da8beefc479
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-11 12:47:10 +0100

    sadsadasd

--- application/controllers/Debug.php
+++ application/controllers/Debug.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Debug extends CI_Controller {
 	public function index()
 	{
-		$this->load->model("userModel");
+		$this->load->model("usermodel");
 		
 		$this->usermodel->createLeerling("riekelt", "test123", "allahhuakbar");
 		

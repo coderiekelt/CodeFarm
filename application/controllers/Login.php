640e4cffd904838e28078798ebfb582da295e2d9
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-12 10:43:43 +0100

    Google Start

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,14 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+include("../libraries/google/src/Google/autoload.php");
 class Login extends CI_Controller {
 	public function index()
 	{
 		$this->load->view("login");
 	}
 	
-	public function layout()
+	public function googledirector()
 	{
-		$this->load->view("header");
+		if (isset($_SESSION['username'])) { return false; }
+		
 	}
 }

c819eac444aa79ebabd21fd0e11fb856f94533e5
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-12 10:49:49 +0100

    test

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-include("../libraries/google/src/Google/autoload.php");
+include(APPPATH."libraries/google/src/Google/autoload.php");
 class Login extends CI_Controller {
 	public function index()
 	{

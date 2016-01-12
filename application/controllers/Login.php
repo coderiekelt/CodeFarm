f5ab6e25e0873957bf8e9bf93794506760d254e6
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-12 10:47:28 +0100

    Google Login Test

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -11,5 +11,20 @@ class Login extends CI_Controller {
 	{
 		if (isset($_SESSION['username'])) { return false; }
 		
+		// DIT MET NIEMAND DELEN
+		$client_id = "690571784639-t8fcgfh35ec0ns4prjl38intv8mkoc02.apps.googleusercontent.com";
+		$client_secret = "VllCcK4yzqzoykW_JSI4bmDj";
+		$redirect_uri = "http://10.100.100.69:8080/login/googleworker";
+		
+		$client = new Google_Client();
+		$client->setClientId($client_id);
+		$client->setClientSecret($client_secret);
+		$client->setRedirectUri($redirect_uri);
+		$client->addScope("email");
+		$client->addScope("profile");
+		
+		$authUrl = $client->createAuthUrl();
+		
+		redirect($authUrl);
 	}
 }

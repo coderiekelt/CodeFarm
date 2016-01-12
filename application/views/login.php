fea427061363d8ba62eb8b7c68260fd9193651f6
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-12 13:09:36 +0100

    Test123

--- application/views/login.php
+++ application/views/login.php
@@ -55,7 +55,7 @@
 													<div class="space"></div>
 
 													<div class="clearfix">
-														<button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+														<button type="submit" style="width: 100%;" class="btn btn-primary">
 															<i class="ace-icon fa fa-key"></i>
 															<span class="bigger-110">Inloggen</span>
 														</button>
@@ -73,7 +73,7 @@
 
 											<div class="social-login center">
 												<a href="login/googledirector">
-													<button class="btn btn-danger">
+													<button style="width: 100%;" class="btn btn-danger">
 														<i class="ace-icon fa fa-google-plus"></i>
 														Login met uw school email!
 													</button>
@@ -88,59 +88,9 @@
 				</div><!-- /.row -->
 			</div><!-- /.main-content -->
 		</div><!-- /.main-container -->
-
-		<!-- basic scripts -->
-
-		<!--[if !IE]> -->
 		<script src="<?php echo asset_url(); ?>js/jquery.js"></script>
-
-		<!-- <![endif]-->
-
-		<!--[if IE]>
-<script src="<?php echo asset_url(); ?>js/jquery1x.js"></script>
-<![endif]-->
 		<script type="text/javascript">
 			if('ontouchstart' in document.documentElement) document.write("<script src='<?php echo asset_url(); ?>js/jquery.mobile.custom.js'>"+"<"+"/script>");
-		</script>
-
-		<!-- inline scripts related to this page -->
-		<script type="text/javascript">
-			jQuery(function($) {
-			 $(document).on('click', '.toolbar a[data-target]', function(e) {
-				e.preventDefault();
-				var target = $(this).data('target');
-				$('.widget-box.visible').removeClass('visible');//hide others
-				$(target).addClass('visible');//show target
-			 });
-			});
-			
-			
-			
-			//you don't need this, just used for changing background
-			jQuery(function($) {
-			 $('#btn-login-dark').on('click', function(e) {
-				$('body').attr('class', 'login-layout');
-				$('#id-text2').attr('class', 'white');
-				$('#id-company-text').attr('class', 'blue');
-				
-				e.preventDefault();
-			 });
-			 $('#btn-login-light').on('click', function(e) {
-				$('body').attr('class', 'login-layout light-login');
-				$('#id-text2').attr('class', 'grey');
-				$('#id-company-text').attr('class', 'blue');
-				
-				e.preventDefault();
-			 });
-			 $('#btn-login-blur').on('click', function(e) {
-				$('body').attr('class', 'login-layout blur-login');
-				$('#id-text2').attr('class', 'white');
-				$('#id-company-text').attr('class', 'light-blue');
-				
-				e.preventDefault();
-			 });
-			 
-			});
 		</script>
 	</body>
 </html>

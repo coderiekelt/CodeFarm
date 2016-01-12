48d6cb15e441cc1b4a75f91ed3e3989e9a7c43a4
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-12 11:17:39 +0100

    dafda

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -27,4 +27,29 @@ class Login extends CI_Controller {
 		
 		redirect($authUrl);
 	}
+	
+	public function googleworker()
+	{
+		if (isset($_SESSION['username'])) { return false; }
+		if (!isset($_GET['code'])) { redirect("googledirector");}
+		// DIT MET NIEMAND DELEN
+		$client_id = "690571784639-t8fcgfh35ec0ns4prjl38intv8mkoc02.apps.googleusercontent.com";
+		$client_secret = "VllCcK4yzqzoykW_JSI4bmDj";
+		$redirect_uri = "http://riekeltbrands.nl/kutding.php";
+		
+		$client = new Google_Client();
+		$client->setClientId($client_id);
+		$client->setClientSecret($client_secret);
+		$client->setRedirectUri($redirect_uri);
+		$client->addScope("email");
+		$client->addScope("profile");
+		
+		$client->authenticate($_GET['code']);
+		$_SESSION['access_token'] = $client->getAccessToken();
+		$client->setAccessToken($_SESSION['access_token']);
+		
+		$user = $service->userinfo->get();
+		
+		echo $user->email;
+	}
 }

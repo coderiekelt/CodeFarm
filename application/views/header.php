491a4f92650a78e6e6c90512a4c1f07c608ae549
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-12 09:47:45 +0100

    Het word al beter

--- application/views/header.php
+++ application/views/header.php
@@ -6,11 +6,11 @@
 		<title>CodeFarm</title>
 		<meta name="description" content="" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-		<link rel="stylesheet" href="<?php echo asset_url(); ?>/css/bootstrap.css" />
-		<link rel="stylesheet" href="<?php echo asset_url(); ?>/css/font-awesome.css" />
-		<link rel="stylesheet" href="<?php echo asset_url(); ?>/css/ace-fonts.css" />
-		<link rel="stylesheet" href="<?php echo asset_url(); ?>/css/ace.css" class="ace-main-stylesheet" id="main-ace-style" />
-		<script src="<?php echo asset_url(); ?>/js/ace-extra.js"></script>
+		<link rel="stylesheet" href="<?php echo asset_url(); ?>css/bootstrap.css" />
+		<link rel="stylesheet" href="<?php echo asset_url(); ?>css/font-awesome.css" />
+		<link rel="stylesheet" href="<?php echo asset_url(); ?>css/ace-fonts.css" />
+		<link rel="stylesheet" href="<?php echo asset_url(); ?>css/ace.css" class="ace-main-stylesheet" id="main-ace-style" />
+		<script src="<?php echo asset_url(); ?>js/ace-extra.js"></script>
 	</head>
 
 	<body class="no-skin">
@@ -209,7 +209,7 @@
 									<ul class="dropdown-menu dropdown-navbar">
 										<li>
 											<a href="#" class="clearfix">
-												<img src="<?php echo asset_url(); ?>/avatars/avatar.png" class="msg-photo" alt="Alex's Avatar" />
+												<img src="<?php echo asset_url(); ?>avatars/avatar.png" class="msg-photo" alt="Alex's Avatar" />
 												<span class="msg-body">
 													<span class="msg-title">
 														<span class="blue">Alex:</span>
@@ -226,7 +226,7 @@
 
 										<li>
 											<a href="#" class="clearfix">
-												<img src="<?php echo asset_url(); ?>/avatars/avatar3.png" class="msg-photo" alt="Susan's Avatar" />
+												<img src="<?php echo asset_url(); ?>avatars/avatar3.png" class="msg-photo" alt="Susan's Avatar" />
 												<span class="msg-body">
 													<span class="msg-title">
 														<span class="blue">Susan:</span>
@@ -243,7 +243,7 @@
 
 										<li>
 											<a href="#" class="clearfix">
-												<img src="<?php echo asset_url(); ?>/avatars/avatar4.png" class="msg-photo" alt="Bob's Avatar" />
+												<img src="<?php echo asset_url(); ?>avatars/avatar4.png" class="msg-photo" alt="Bob's Avatar" />
 												<span class="msg-body">
 													<span class="msg-title">
 														<span class="blue">Bob:</span>
@@ -260,7 +260,7 @@
 
 										<li>
 											<a href="#" class="clearfix">
-												<img src="<?php echo asset_url(); ?>/avatars/avatar2.png" class="msg-photo" alt="Kate's Avatar" />
+												<img src="<?php echo asset_url(); ?>avatars/avatar2.png" class="msg-photo" alt="Kate's Avatar" />
 												<span class="msg-body">
 													<span class="msg-title">
 														<span class="blue">Kate:</span>
@@ -277,7 +277,7 @@
 
 										<li>
 											<a href="#" class="clearfix">
-												<img src="<?php echo asset_url(); ?>/avatars/avatar5.png" class="msg-photo" alt="Fred's Avatar" />
+												<img src="<?php echo asset_url(); ?>avatars/avatar5.png" class="msg-photo" alt="Fred's Avatar" />
 												<span class="msg-body">
 													<span class="msg-title">
 														<span class="blue">Fred:</span>
@@ -306,7 +306,7 @@
 						<!-- #section:basics/navbar.user_menu -->
 						<li class="light-blue">
 							<a data-toggle="dropdown" href="#" class="dropdown-toggle">
-								<img class="nav-user-photo" src="<?php echo asset_url(); ?>/avatars/user.jpg" alt="Jason's Photo" />
+								<img class="nav-user-photo" src="<?php echo asset_url(); ?>avatars/user.jpg" alt="Jason's Photo" />
 								<span class="user-info">
 									<small>Welcome,</small>
 									Jason
@@ -1058,52 +1058,52 @@
 		<!-- basic scripts -->
 
 		<!--[if !IE]> -->
-		<script src="<?php echo asset_url(); ?>/js/jquery.js"></script>
+		<script src="<?php echo asset_url(); ?>js/jquery.js"></script>
 
 		<!-- <![endif]-->
 
 		<!--[if IE]>
-<script src="<?php echo asset_url(); ?>/js/jquery1x.js"></script>
+<script src="<?php echo asset_url(); ?>js/jquery1x.js"></script>
 <![endif]-->
 		<script type="text/javascript">
-			if('ontouchstart' in document.documentElement) document.write("<script src='<?php echo asset_url(); ?>/js/jquery.mobile.custom.js'>"+"<"+"/script>");
+			if('ontouchstart' in document.documentElement) document.write("<script src='<?php echo asset_url(); ?>js/jquery.mobile.custom.js'>"+"<"+"/script>");
 		</script>
-		<script src="<?php echo asset_url(); ?>/js/bootstrap.js"></script>
+		<script src="<?php echo asset_url(); ?>js/bootstrap.js"></script>
 
 		<!-- page specific plugin scripts -->
 
 		<!-- ace scripts -->
-		<script src="<?php echo asset_url(); ?>/js/ace/elements.scroller.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/elements.colorpicker.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/elements.fileinput.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/elements.typeahead.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/elements.wysiwyg.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/elements.spinner.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/elements.treeview.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/elements.wizard.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/elements.aside.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/ace.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/ace.ajax-content.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/ace.touch-drag.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/ace.sidebar.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/ace.sidebar-scroll-1.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/ace.submenu-hover.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/ace.widget-box.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/ace.settings.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/ace.settings-rtl.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/ace.settings-skin.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/ace.widget-on-reload.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/ace.searchbox-autocomplete.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.scroller.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.colorpicker.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.fileinput.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.typeahead.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.wysiwyg.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.spinner.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.treeview.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.wizard.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.aside.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.ajax-content.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.touch-drag.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.sidebar.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.sidebar-scroll-1.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.submenu-hover.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.widget-box.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.settings.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.settings-rtl.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.settings-skin.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.widget-on-reload.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.searchbox-autocomplete.js"></script>
 
 		<!-- inline scripts related to this page -->
 
 		<!-- the following scripts are used in demo only for onpage help and you don't need them -->
-		<link rel="stylesheet" href="<?php echo asset_url(); ?>/css/ace.onpage-help.css" />
+		<link rel="stylesheet" href="<?php echo asset_url(); ?>css/ace.onpage-help.css" />
 		<link rel="stylesheet" href="../docs/assets/js/themes/sunburst.css" />
 
 		<script type="text/javascript"> ace.vars['base'] = '..'; </script>
-		<script src="<?php echo asset_url(); ?>/js/ace/elements.onpage-help.js"></script>
-		<script src="<?php echo asset_url(); ?>/js/ace/ace.onpage-help.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.onpage-help.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.onpage-help.js"></script>
 		<script src="../docs/assets/js/rainbow.js"></script>
 		<script src="../docs/assets/js/language/generic.js"></script>
 		<script src="../docs/assets/js/language/html.js"></script>

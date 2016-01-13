34a1754f5ed9161d544ebe4bd5ae8bd8b62487d3
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-13 11:24:21 +0100

    oeps

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 include(APPPATH."libraries/Google/vendor/autoload.php");
 
-class Login extends CI_Controller {
+class Dashboard extends CI_Controller {
 	public function index()
 	{
 		$this->load->view("header");

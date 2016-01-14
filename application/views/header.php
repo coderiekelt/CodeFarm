6db255c0adacc9e77db7a658c89ce720ce8c784b
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-14 15:36:49 +0100

    test

--- application/views/header.php
+++ application/views/header.php
@@ -16,6 +16,42 @@
 		<link rel="stylesheet" href="<?php echo asset_url(); ?>css/ace-fonts.css" />
 		<link rel="stylesheet" href="<?php echo asset_url(); ?>css/ace.css" class="ace-main-stylesheet" id="main-ace-style" />
 		<script src="<?php echo asset_url(); ?>js/ace-extra.js"></script>
+		<!-- basic scripts -->
+		<!--[if !IE]> -->
+		<script src="<?php echo asset_url(); ?>js/jquery.js"></script>
+		<!-- <![endif]-->
+		<!--[if IE]>
+		<script src="<?php echo asset_url(); ?>js/jquery1x.js"></script>
+		<![endif]-->
+		<script type="text/javascript">
+			if('ontouchstart' in document.documentElement) document.write("<script src='<?php echo asset_url(); ?>js/jquery.mobile.custom.js'>"+"<"+"/script>");
+		</script>
+		<script src="<?php echo asset_url(); ?>js/bootstrap.js"></script>
+		<!-- page specific plugin scripts -->
+		<!-- ace scripts -->
+		<script src="<?php echo asset_url(); ?>js/ace/elements.scroller.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.colorpicker.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.fileinput.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.typeahead.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.wysiwyg.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.spinner.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.treeview.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.wizard.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/elements.aside.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.ajax-content.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.touch-drag.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.sidebar.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.sidebar-scroll-1.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.submenu-hover.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.widget-box.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.settings.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.settings-rtl.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.settings-skin.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.widget-on-reload.js"></script>
+		<script src="<?php echo asset_url(); ?>js/ace/ace.searchbox-autocomplete.js"></script>
+		<script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
+		<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
 	</head>
 
 	<body class="no-skin">

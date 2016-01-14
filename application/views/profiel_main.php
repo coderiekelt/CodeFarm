3d8761b7c133a39e95bd775890ccc3b90cf10fd2
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-14 09:53:06 +0100

    Meer dingen

--- application/views/profiel_main.php
+++ application/views/profiel_main.php
@@ -30,80 +30,31 @@
 						</h4>
 
 						<div class="profile-user-info">
-								<div class="profile-info-row">
-									<div class="profile-info-name"> Gebruikersnaam </div>
-
-									<div class="profile-info-value">
-										<span><?php echo $gebruiker['gebruikersnaam']; ?></span>
-									</div>
+							<div class="profile-info-row">
+								<div class="profile-info-name"> Gebruikersnaam </div>
+								<div class="profile-info-value">
+									<span><?php echo $gebruiker['gebruikersnaam']; ?></span>
 								</div>
-
-																<div class="profile-info-row">
-																	<div class="profile-info-name"> Location </div>
-
-																	<div class="profile-info-value">
-																		<i class="fa fa-map-marker light-orange bigger-110"></i>
-																		<span>Netherlands</span>
-																		<span>Amsterdam</span>
-																	</div>
-																</div>
-
-																<div class="profile-info-row">
-																	<div class="profile-info-name"> Age </div>
-
-																	<div class="profile-info-value">
-																		<span>38</span>
-																	</div>
-																</div>
-
-																<div class="profile-info-row">
-																	<div class="profile-info-name"> Joined </div>
-
-																	<div class="profile-info-value">
-																		<span>2010/06/20</span>
-																	</div>
-																</div>
-
-																<div class="profile-info-row">
-																	<div class="profile-info-name"> Last Online </div>
-
-																	<div class="profile-info-value">
-																		<span>3 hours ago</span>
-																	</div>
-																</div>
-															</div>
-
-															<div class="hr hr-8 dotted"></div>
-
-															<div class="profile-user-info">
-																<div class="profile-info-row">
-																	<div class="profile-info-name"> Website </div>
-
-																	<div class="profile-info-value">
-																		<a href="#" target="_blank">www.alexdoe.com</a>
-																	</div>
-																</div>
-
-																<div class="profile-info-row">
-																	<div class="profile-info-name">
-																		<i class="middle ace-icon fa fa-facebook-square bigger-150 blue"></i>
-																	</div>
-
-																	<div class="profile-info-value">
-																		<a href="#">Find me on Facebook</a>
-																	</div>
-																</div>
-
-																<div class="profile-info-row">
-																	<div class="profile-info-name">
-																		<i class="middle ace-icon fa fa-twitter-square bigger-150 light-blue"></i>
-																	</div>
-
-																	<div class="profile-info-value">
-																		<a href="#">Follow me on Twitter</a>
-																	</div>
-																</div>
-															</div>
+							</div>
+							<div class="profile-info-row">
+								<div class="profile-info-name"> Voornaam </div>
+								<div class="profile-info-value">
+									<span><?php echo $gebruiker['voornaam']; ?></span>
+								</div>
+							</div>
+							<div class="profile-info-row">
+								<div class="profile-info-name"> Achternaam </div>
+								<div class="profile-info-value">
+									<span><?php echo $gebruiker['achternaam']; ?></span>
+								</div>
+							</div>
+							<div class="profile-info-row">
+								<div class="profile-info-name"> E-mail adres </div>
+								<div class="profile-info-value">
+									<span><?php echo $gebruiker['email']; ?></span>
+								</div>
+							</div>				
+						</div>
 														</div><!-- /.col -->
 													</div><!-- /.row -->
 

b6f2518db4f792150351e162468ffeac445c8d94
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-14 15:29:05 +0100

    test

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -21,6 +21,10 @@ class Dashboard extends CI_Controller {
 	
 	public function addgroup($process = "false")
 	{
+		if (!isset($_SESSION['usernaam'])) { redirect("login"); }
+		if (!$_SESSION['domein'] == "beheerder") { exit; }
+		
+		$this->load->model("gebruiker");
 		$this->load->view("dash_admin_group");
 	}
 }

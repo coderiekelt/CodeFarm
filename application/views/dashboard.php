bde42af145b0586b5d0d016a40016a22f4716a18
Klaas Hakvoort <hakvoort.k98@gmail.com>
2016-01-14 11:29:43 +0100

    Sorry

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -2,7 +2,7 @@
 <?php if (isset($koppelfout)) { ?>
 	<div class="alert alert-danger">
 		<b>Foutmelding!</b><br>
-		Uw account is nog niet gekoppeld aan een klas, hierdoor hebben wij geen trajecten kunnen test ophalen.<br>
+		Uw account is nog niet gekoppeld aan een klas, hierdoor hebben wij geen trajecten kunnen ophalen.<br>
 		Vraag aan uw docent om u aan de correcte klas te koppelen.
 	</div>
 <?php } else { ?>

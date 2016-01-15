b071c72b04713c17b50abc60143ac7836260ddef
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-15 11:09:34 +0100

    e

--- application/views/klassenbeheer_main.php
+++ application/views/klassenbeheer_main.php
@@ -1,25 +1,21 @@
-<div class="container">
-	<span class="pull-left" style="font-size: 16pt; font-weight: bold;">
-		Klassenbeheer
-	</span>
-	<span class="pull-right">
-		<a href="<?php echo site_url("klassenbeheer/nieuw"); ?>">
-			<button type="button" class="btn btn-success">
-				<i class="ace-icon fa fa-plus"></i> 
-				Nieuwe klas
-			</button>
-		</a>
-	</span>
-</div>
+<span style="font-size: 16pt; font-weight: bold;">
+	Klassenbeheer
+</span>
+<span class="pull-right">
+	<a href="<?php echo site_url("klassenbeheer/nieuw"); ?>">
+		<button type="button" class="btn btn-success">
+			<i class="ace-icon fa fa-plus"></i> 
+			Nieuwe klas
+		</button>
+	</a>
+</span>
 <?php if (!isset($klassen))
 {
 	?>
-	<div class="container">
-		<div class="alert alert-danger">
-			<b>Foutmelding</b><br>
-			Wij hebben helaas geen klassen kunnen ophalen, dit komt omdat er helaas nog geen klassen bestaan.<br>
-			Klik op de knop "Nieuwe klas" om een nieuwe klas aan te maken!
-		</div>
+	<div class="alert alert-danger">
+		<b>Foutmelding</b><br>
+		Wij hebben helaas geen klassen kunnen ophalen, dit komt omdat er helaas nog geen klassen bestaan.<br>
+		Klik op de knop "Nieuwe klas" om een nieuwe klas aan te maken!
 	</div>
 	<?php
 } ?>

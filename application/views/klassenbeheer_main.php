d18812040cc726402c90f041c56a43d90a03fc64
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-15 11:06:06 +0100

    test

--- application/views/klassenbeheer_main.php
+++ application/views/klassenbeheer_main.php
@@ -12,8 +12,10 @@
 <?php if (!isset($klassen))
 {
 	?>
-	<div class="alert alert-danger"><b>Foutmelding</b><br>
-	Wij hebben helaas geen klassen kunnen ophalen, dit komt omdat er helaas nog geen klassen bestaan.<br>
-	Klik op de knop "Nieuwe klas" om een nieuwe klas aan te maken!
+	<div class="alert alert-danger">
+		<b>Foutmelding</b><br>
+		Wij hebben helaas geen klassen kunnen ophalen, dit komt omdat er helaas nog geen klassen bestaan.<br>
+		Klik op de knop "Nieuwe klas" om een nieuwe klas aan te maken!
+	</div>
 	<?php
 } ?>

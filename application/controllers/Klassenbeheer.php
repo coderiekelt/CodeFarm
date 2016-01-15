16a2ddbe690f852ce51ab7447e062f66e2072a53
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-15 10:47:57 +0100

    KAAS

---
+++ application/controllers/Klassenbeheer.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+include(APPPATH."libraries/Google/vendor/autoload.php");
+
+class Klassenbeheer extends CI_Controller {
+	public function __construct()
+	{
+		if (!isset($_SESSION['usernaam']))
+		{
+			redirect("login");
+		} elseif (!$_SESSION['domein'] == "beheerder")
+		{
+			redirect("dashboard");
+		}
+	}
+	
+	public function index()
+	{
+		$this->load->view("header", array("title" => "Klassen beheer"));
+		
+		$this->load->view("footer");
+	}
+}

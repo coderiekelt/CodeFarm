02de7217f87fd8bbcbc809484a92b00e39fd78ff
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-19 14:06:07 +0100

    woot

--- application/views/gebruikers_lijst.php
+++ application/views/gebruikers_lijst.php
@@ -1,7 +1,6 @@
 <h3>Gebruikers <small><?php echo $domein; ?></h3>
 <table class="table table-bordered">
 	<tr>
-		<th>#</th>
 		<th>Gebruikersnaam</th>
 		<th>Voornaam</th>
 		<th>Achternaam</th>
@@ -9,6 +8,14 @@
 		<th>Acties</th>
 	</tr>
 	<?php
-		
+		foreach($gebruikers as $gebruiker)
+		{
+			print("<tr>");
+				print('<td>' . $gebruiker->gebruikersnaam . '</td>');
+				print('<td>' . $gebruiker->voornaam . '</td>');
+				print('<td>' . $gebruiker->achternaam . '</td>');
+				print('<td>' . $gebruiker->datum_registratie . '</td>');
+			print("</tr>");
+		}
 	?>
 </table>

4f2ebed6b5cedec32100ad1cf944fca9e71cec3f
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-19 14:03:56 +0100

    testen

--- application/views/gebruikers_lijst.php
+++ application/views/gebruikers_lijst.php
@@ -1,4 +1,14 @@
 <h3>Gebruikers <small><?php echo $domein; ?></h3>
 <table class="table table-bordered">
-
+	<tr>
+		<th>#</th>
+		<th>Gebruikersnaam</th>
+		<th>Voornaam</th>
+		<th>Achternaam</th>
+		<th>Registratie Datum</th>
+		<th>Acties</th>
+	</tr>
+	<?php
+		
+	?>
 </table>

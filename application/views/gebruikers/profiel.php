025b06a359239119a71f7015563e40fa9a1cd2fe
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-21 10:47:07 +0100

    e

--- application/views/gebruikers/profiel.php
+++ application/views/gebruikers/profiel.php
@@ -1,5 +1,9 @@
 <div class="page-header">
 	<h1>
 		<?php echo $gebruiker->voornaam . " " . $gebruiker->achternaam; ?>
+		<small>
+			<i class="ace-icon fa fa-angle-double-right"></i>
+			<?php echo $gebruiker->gebruikersnaam; ?>
+		</small>
 	</h1>
 </div>

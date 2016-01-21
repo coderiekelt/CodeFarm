ceb8d6bf0f3951b315a4ff4fb97c660cc0cbf556
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-21 11:15:15 +0100

    baeaea

--- application/views/gebruikers/bewerk.php
+++ application/views/gebruikers/bewerk.php
@@ -8,46 +8,6 @@
 	</h1>
 </div>
 <?php echo form_open("gebruikers/create/confirm"); ?>
-<script>
-	function randomPassword(length) {
-    	var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOP1234567890";
-    	var pass = "";
-    	for (var x = 0; x < length; x++) {
-     	   var i = Math.floor(Math.random() * chars.length);
-    	    pass += chars.charAt(i);
-    	}
-    	return pass;
-	}
-
-	$(document).ready(function() {
-		$("#passInfo").hide();
-
-		$("#genPass").click(function()
-		{
-			var pass = randomPassword(12);
-			$("input[name='wachtwoord']").val(pass);
-			$("#passInfo").text("Uw nieuwe wachtwoord is: " + pass);
-			$("#passInfo").show();
-		});
-	});
-</script>
-<div>
-	<label>
-		Wachtwoord: <small>laat dit leeg om dit niet te veranderen</small>
-	</label>
-
-	<div class="input-group" style="width: 100%">
-		<input class="form-control" type="password" name="newwachtwoord" value=""/>
-		<span class="input-group-btn">
-			<button class="btn btn-sm btn-primary" id="genPass" type="button">
-				<i class="ace-icon fa fa-gears bigger-110"></i>
-				Genereer
-			</button>
-		</span>
-	</div>
-</div>
-<div class="alert alert-info" id="passInfo"></div>
-<br>
 <div>
 	<label>
 		Voornaam:

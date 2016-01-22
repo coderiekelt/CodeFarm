1a442905ca4d6c8384db146a32c52c8c6c10b877
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-22 10:52:58 +0100

    sowwie

--- application/views/klassen/nieuw.php
+++ application/views/klassen/nieuw.php
@@ -23,16 +23,7 @@
 		available.forEach(function (entry)
 		{
 			var sn = "moveToPart('" + entry + "')";
-			$("#availableStudents").append('<tr>
-							/<td>
-								/'+ entry +'
-							/</td>
-							/<td>
-								/<button onclick="'+ sn +'" class="btn btn-minier btn-danger dropdown-toggle">
-									/<i class="ace-icon fa fa-remove icon-only bigger-120"></i>
-								/</button>
-							/</td>
-						/</tr>');
+			$("#availableStudents").append('<tr><td>'+ entry +'</td><td><button onclick="'+ sn +'" class="btn btn-minier btn-danger dropdown-toggle"><i class="ace-icon fa fa-remove icon-only bigger-120"></i></button</td></tr>');
 		});
 	}
 

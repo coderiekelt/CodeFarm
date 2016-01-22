99b730f275109edee20a9ecca265d13933363e5f
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-22 10:55:23 +0100

    aas

--- application/views/klassen/nieuw.php
+++ application/views/klassen/nieuw.php
@@ -23,7 +23,7 @@
 		available.forEach(function (entry)
 		{
 			var sn = "moveToPart('" + entry + "')";
-			$("#availableStudents").append('<tr><td>'+ entry +'</td><td><button onclick="'+ sn +'" class="btn btn-minier btn-success dropdown-toggle"><i class="ace-icon fa fa-angle-double-right icon-only bigger-120"></i></button</td></tr>');
+			$("#availableStudents").append('<tr><td>'+ entry +'</td><td><button type="button" onclick="'+ sn +'" class="btn btn-minier btn-success dropdown-toggle"><i class="ace-icon fa fa-angle-double-right icon-only bigger-120"></i></button</td></tr>');
 		});
 	}
 

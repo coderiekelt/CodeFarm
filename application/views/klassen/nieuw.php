3808ce235b89eadb30a07ff0e56e1dbd28237303
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-22 10:31:23 +0100

    deelnemers

--- application/views/klassen/nieuw.php
+++ application/views/klassen/nieuw.php
@@ -3,7 +3,7 @@
 		Nieuwe klas toevoegen
 	</h1>
 </div>
-<?php echo form_open("klas/create/confirm"); ?>
+<?php echo form_open("klassen/create/confirm"); ?>
 <div>
 	<label>
 		Klas naam:
@@ -12,6 +12,12 @@
 	<div class="input-group" style="width: 100%">
 		<input class="form-control" type="text" name="naam" />
 	</div>
+</div>
+<div>
+	<label>
+		Deelnemers:
+		<small>Waarschuwing! Als een deelnemer al in een klas zit, wordt deze toegewezen aan de nieuwe klas.</small>
+	</label>
 </div>
 <br>
 <button type="submit" class="btn btn-primary"><i class="ace-icon fa fa-plus"></i> Klas aanmaken</button>

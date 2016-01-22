e184347657e580d05c3dc6dcdd3af7c7e63df140
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-22 10:16:21 +0100

    klasverwd

---
+++ application/views/klassen/verwijder.php
@@ -0,0 +1,18 @@
+<div class="page-header">
+	<h1>
+		Verwijder <?php echo $klas; ?>
+	</h1>
+</div>
+<div class="panel panel-danger">
+	<div class="panel-heading">
+		U staat op het punt om een actie te ondernemen die niet ongedaan gemaakt kan worden!
+	</div>
+	<div class="panel-body">
+		Weet u zeker dat u deze gebruiker wilt verwijderen?<br>
+		Deze actie kan <b>niet</b> ongedaan gemaakt worden!<br>
+		<br>
+		<a href="<?php echo site_url("klassen/edit/" . $klas); ?>">Nee, laat mij deze klas bewerken.</a><br>
+		<a href="<?php echo site_url("klassen/lijst"); ?>">Nee, breng mij terug naar de klassenlijst.</a><br>
+		<a href="<?php echo site_url("klassen/delete/" . $klas ."/confirm"); ?>">Ja, verwijder deze klas.</a>
+	</div>
+</div>

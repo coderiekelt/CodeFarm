774582b4527ef4c8f02caad9949a125b4750717c
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-22 10:32:28 +0100

    deelnemertjes

--- application/views/klassen/nieuw.php
+++ application/views/klassen/nieuw.php
@@ -13,11 +13,24 @@
 		<input class="form-control" type="text" name="naam" />
 	</div>
 </div>
+<br>
 <div>
 	<label>
 		Deelnemers:
 		<small>Waarschuwing! Als een deelnemer al in een klas zit, wordt deze toegewezen aan de nieuwe klas.</small>
 	</label>
+	<div class="row">
+		<div class="col-xs-8">
+			<div class="well">
+				Hier komen de beschikbaar deelnemers
+			</div>
+		</div>
+		<div class="col-xs-8">
+			<div class="well">
+				Hier komen de deelnemers van de klas
+			</div>
+		</div>
+	</div>
 </div>
 <br>
 <button type="submit" class="btn btn-primary"><i class="ace-icon fa fa-plus"></i> Klas aanmaken</button>

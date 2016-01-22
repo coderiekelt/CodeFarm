d09d1dfc1382819325a45be8eca74df352163579
Riekelt Brands <riekelturk@yahoo.nl>
2016-01-22 10:51:07 +0100

    eeescape

--- application/views/klassen/nieuw.php
+++ application/views/klassen/nieuw.php
@@ -24,15 +24,15 @@
 		{
 			var sn = "moveToPart('" + entry + "')";
 			$("#availableStudents").append('<tr>
-							<td>
-								'+ entry +'
-							</td>
-							<td>
-								<button onclick="'+ sn +'" class="btn btn-minier btn-danger dropdown-toggle">
-									<i class="ace-icon fa fa-remove icon-only bigger-120"></i>
-								</button>
-							</td>
-						</tr>');
+							/<td>
+								/'+ entry +'
+							/</td>
+							/<td>
+								/<button onclick="'+ sn +'" class="btn btn-minier btn-danger dropdown-toggle">
+									/<i class="ace-icon fa fa-remove icon-only bigger-120"></i>
+								/</button>
+							/</td>
+						/</tr>');
 		});
 	}
 

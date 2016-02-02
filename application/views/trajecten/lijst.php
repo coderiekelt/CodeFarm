39c7f44a6731fd9a444cc62b1b24ed2799583958
Klaas Hakvoort <hakvoort.k98@gmail.com>
2016-02-02 13:30:21 +0100

    dag 2 ik ben hier niet alleen

--- application/views/trajecten/lijst.php
+++ application/views/trajecten/lijst.php
@@ -6,7 +6,7 @@
 <div class="row">
 	<?php foreach($projecten as $project) {
 		$data = $this->traject->fetchdetails($traject->traject_id);
-		print('<a href="' . site_url("projecten/view/" . $project->project_id) . '">');
+		print('<a href="' . site_url("trajecten/view/" . $traject->traject_id) . '">');
 			print('<div class="col-xs-2">');
 				print('<div class="well">');
 					print('<center><img style="width: 100%; height: 100%;" src="' . $data->thumb . '" /></center>');

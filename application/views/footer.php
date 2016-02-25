8e9e9f6f944e9b81077fe53a9e84bf3569c81a16
Riekelt Brands <riekelturk@yahoo.nl>
2016-02-25 21:11:32 +0100

    Symfony base

--- application/views/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- PAGE CONTENT ENDS -->
-							</div><!-- /.col -->
-						</div><!-- /.row -->
-					</div><!-- /.page-content -->
-				</div>
-			</div><!-- /.main-content -->
-		</div><!-- /.main-container -->
-	</body>
-</html>
--- application/views/projecten/theorie.php
+++
@@ -1,6 +0,0 @@
-<div class="page-header">
-	<h1>
-		<?php echo $theorie->naam; ?>
-	</h1>
-</div>
-<?php echo $theorie->content; ?>

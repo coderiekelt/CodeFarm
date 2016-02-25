93a72fd87f2c5a58caded9baf7a102caa6fe9265
Riekelt Brands <riekelturk@yahoo.nl>
2016-02-25 22:41:19 +0100

    Layout base

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,16 +14,8 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('@App/dashboard/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
-    }
-
-    /**
-     * @Route("/test", name="test")
-     */
-    public function testAction()
-    {
-        return $this->render('AppBundle::base.html.twig', []);
     }
 }
